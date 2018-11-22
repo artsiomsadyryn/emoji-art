ec3e4f155e516126e08c32a9f8234f4386c7bccb
Artyom Sadyrin <asadyryn@icloud.com>
2018-11-22 17:31:41 +0300

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Artyom Sadyrin on 11/22/18.
+//  Copyright © 2018 Artyom Sadyrin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
