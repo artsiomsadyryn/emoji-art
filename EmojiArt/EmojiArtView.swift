c658a014dfea105c2a0cc397e5588efa14e0ef6a
Artyom Sadyrin <artsiomsadyryn@gmail.com>
2018-11-23 15:37:25 +0300

    Created a EmojiArtView for custom UIView

---
+++ EmojiArt/EmojiArtView.swift
@@ -0,0 +1,21 @@
+//
+//  EmojiArtView.swift
+//  EmojiArt
+//
+//  Created by Artyom Sadyrin on 11/23/18.
+//  Copyright © 2018 Artyom Sadyrin. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
