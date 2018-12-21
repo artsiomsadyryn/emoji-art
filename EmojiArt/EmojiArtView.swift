bc5fcee51e3abbbfb83815215d0c966642dae2e8
Artyom Sadyrin <artsiomsadyryn@gmail.com>
2018-12-21 18:48:45 +0300

    Implemented a drop from collection view to emojiArtView

--- EmojiArt/EmojiArtView.swift
+++ EmojiArt/EmojiArtView.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class EmojiArtView: UIView
+class EmojiArtView: UIView, UIDropInteractionDelegate
 {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        addInteraction(UIDropInteraction(delegate: self))
+    }
+    
     
     // MARK: Properties
     
@@ -18,10 +33,42 @@ class EmojiArtView: UIView
             setNeedsDisplay()
         }
     }
+    
+    // MARK: Methods
 
     override func draw(_ rect: CGRect) {
         // Drawing code
         backgroundImage?.draw(in: bounds)
     }
+    
+    private func addLabel(with attributedString: NSAttributedString, centeredAt point: CGPoint) {
+        let label = UILabel()
+        label.backgroundColor = .clear
+        label.attributedText = attributedString
+        label.sizeToFit()
+        label.center = point
+        addSubview(label )
+    }
+    
+    // MARK: Drop from Collection View Methods
+    
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSAttributedString.self)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        session.loadObjects(ofClass: NSAttributedString.self) { (providers) in
+            let dropPoint = session.location(in: self)
+            for attributedString in providers as? [NSAttributedString] ?? [] {
+                self.addLabel(with: attributedString, centeredAt: dropPoint)
+            }
+        }
+    }
+    
+    
 
 }
