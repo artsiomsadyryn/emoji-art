5029688eaabbf8c0633a95a5968b6cd17fa919f6
Artyom Sadyrin <artsiomsadyryn@gmail.com>
2019-01-11 18:23:29 +0300

    Created Document Based App and moved old files there

--- EmojiArt/Supporting Files/AppDelegate.swift
+++ EmojiArt/Supporting Files/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  EmojiArt
 //
-//  Created by Artyom Sadyrin on 11/22/18.
-//  Copyright © 2018 Artyom Sadyrin. All rights reserved.
+//  Created by Artyom Sadyrin on 1/11/19.
+//  Copyright © 2019 Artyom Sadyrin. All rights reserved.
 //
 
 import UIKit
@@ -39,6 +39,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    func application(_ app: UIApplication, open inputURL: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        // Ensure the URL is a file URL
+        guard inputURL.isFileURL else { return false }
+                
+        // Reveal / import the document at the URL
+        guard let documentBrowserViewController = window?.rootViewController as? DocumentBrowserViewController else { return false }
+
+        documentBrowserViewController.revealDocument(at: inputURL, importIfNeeded: true) { (revealedDocumentURL, error) in
+            if let error = error {
+                // Handle the error appropriately
+                print("Failed to reveal the document at URL \(inputURL) with error: '\(error)'")
+                return
+            }
+            
+            // Present the Document View Controller for the revealed URL
+            documentBrowserViewController.presentDocument(at: revealedDocumentURL!)
+        }
+
+        return true
     }
 
 
